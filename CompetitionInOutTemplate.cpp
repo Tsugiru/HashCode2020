fad7f6978a73780d19bb811bb91d61bc5eae0d3a
SULTANEM Ryan <rsultanem@murex.com>
2020-02-20 20:08:35 +0200

    InputOutput

--- CompetitionInOutTemplate.cpp
+++ CompetitionInOutTemplate.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-<<<<<<< HEAD
 #include <fstream>
 #include <vector>
 #include <unordered_map>
@@ -11,29 +10,66 @@
 #include <utility>
 
 using namespace std;
-=======
->>>>>>> e56a647ce745a6eef9d28bff367faac7e67f7474
+
+struct Library
+{
+	int numberOfBooks;
+	std::vector<int> bookIds;
+
+	int processDays;
+	int shippingDays;
+};
 
 struct Input
 {
-	// Fill Input info needed
+	int numberOfBooks;
+	int numberOfLibraries;
+	int scanDays;
+
+	std::vector<int> bookScores;
+	std::vector<Library> libraries;
+};
+
+struct LibraryOutput
+{
+	int id;
+	int numberOfBooksForScanning = 0;
+	std::vector<int> bookIds;
 };
 
 struct Result
 {
 	// Fill Output info needed
 
+	int numberOfLibraries = 0;
+
+	std::vector<LibraryOutput> librariesOutputs;
+
 	std::string toString() const
 	{
 		// Fill Format of output needed ( it will be appended to Case #, in case not needed check the solve function)
-		return "";
+		std::string result;
+
+		result.append(to_string(numberOfLibraries));
+		result.append("\n");
+
+		for (int i = 0; i < librariesOutputs.size(); ++i)
+		{
+			LibraryOutput libraryOutput = librariesOutputs[i];
+			result.append(to_string(libraryOutput.id) + " " + to_string(libraryOutput.bookIds.size()));
+			result.append("\n");
+
+			for (int j = 0; j < libraryOutput.bookIds.size(); ++j)
+			{
+				result.append(to_string(libraryOutput.bookIds[j]) + " ");
+			}
+			result.append("\n");
+		}
+
+		return result;
 	}
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e56a647ce745a6eef9d28bff367faac7e67f7474
 Result solveCase(const Input & input)
 {
 	Result result;
@@ -47,6 +83,35 @@ Input getInputs()
 {
 	Input input;
 
+	cin >> input.numberOfBooks;
+	cin >> input.numberOfLibraries;
+	cin >> input.scanDays;
+
+	input.bookScores.reserve(input.numberOfBooks);
+	for (int i = 0; i < input.numberOfBooks; ++i)
+	{
+		int bookScore;
+		cin >> bookScore;
+		input.bookScores.push_back(bookScore);
+	}
+
+	for (int i = 0; i < input.numberOfLibraries; ++i)
+	{
+		Library	library;
+		cin >> library.numberOfBooks;
+		cin >> library.processDays;
+		cin >> library.shippingDays;
+
+		for (int j = 0; j < library.numberOfBooks; ++j)
+		{
+			int bookId;
+			cin >> bookId;
+			library.bookIds.push_back(bookId);
+		}
+		
+		input.libraries.push_back(library);
+	}
+
 	// Fill in the input with std::cin >>..
 
 	// std::cin >> ....;
@@ -54,29 +119,26 @@ Input getInputs()
 	return input;
 }
 
-void solve(int caseNumber)
+void solve(int /*caseNumber*/)
 {
 	Input input = getInputs();
 	Result result = solveCase(input);
-	std::cout << "Case #" << caseNumber << ": " << result.toString() << std::endl;
+	//std::cout << "Case #" << caseNumber << ": " << result.toString() << std::endl;
+	
+	std::cout << result.toString() << std::endl;
 }
 
-<<<<<<< HEAD
 int main()
 {
-    
-=======
-
-int main()
-{
-	int numberOfTestCases = 0;
+	/*int numberOfTestCases = 0;
 	std::cin >> numberOfTestCases;
 
 	for (int i = 0; i < numberOfTestCases; ++i)
 	{
 		solve(i + 1);
-	}
+	}*/
 
->>>>>>> e56a647ce745a6eef9d28bff367faac7e67f7474
+	solve(0);
+
 	return 0;
 }
