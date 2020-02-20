709b98cc31a8387d66af2700046bea25262f5219
tsugiru <eliemaamary17@gmail.com>
2020-02-20 19:15:01 +0200

    add includes

---
+++ CompetitionInOutTemplate.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+struct Input
+{
+	// Fill Input info needed
+};
+
+struct Result
+{
+	// Fill Output info needed
+
+	std::string toString() const
+	{
+		// Fill Format of output needed ( it will be appended to Case #, in case not needed check the solve function)
+		return "";
+	}
+};
+
+Result solveCase(const Input & input)
+{
+	Result result;
+	
+	// The actual solution goes here.. while filling "result", and reading input from "input"
+
+	return result;
+}
+
+Input getInputs()
+{
+	Input input;
+
+	// Fill in the input with std::cin >>..
+
+	// std::cin >> ....;
+
+	return input;
+}
+
+void solve(int caseNumber)
+{
+	Input input = getInputs();
+	Result result = solveCase(input);
+	std::cout << "Case #" << caseNumber << ": " << result.toString() << std::endl;
+}
+
+int main()
+{
+    
+
+	return 0;
+}
