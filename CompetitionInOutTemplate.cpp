5b55f8e8dd5daca7176640a7aac52e830b3f5428
tsugiru <eliemaamary17@gmail.com>
2020-02-20 20:06:45 +0200

    add test cases

--- CompetitionInOutTemplate.cpp
+++ CompetitionInOutTemplate.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-<<<<<<< HEAD
 #include <fstream>
 #include <vector>
 #include <unordered_map>
@@ -11,8 +10,6 @@
 #include <utility>
 
 using namespace std;
-=======
->>>>>>> e56a647ce745a6eef9d28bff367faac7e67f7474
 
 struct Input
 {
@@ -30,10 +27,6 @@ struct Result
 	}
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e56a647ce745a6eef9d28bff367faac7e67f7474
 Result solveCase(const Input & input)
 {
 	Result result;
@@ -58,25 +51,11 @@ void solve(int caseNumber)
 {
 	Input input = getInputs();
 	Result result = solveCase(input);
-	std::cout << "Case #" << caseNumber << ": " << result.toString() << std::endl;
 }
 
-<<<<<<< HEAD
 int main()
 {
-    
-=======
-
-int main()
-{
-	int numberOfTestCases = 0;
-	std::cin >> numberOfTestCases;
-
-	for (int i = 0; i < numberOfTestCases; ++i)
-	{
-		solve(i + 1);
-	}
-
->>>>>>> e56a647ce745a6eef9d28bff367faac7e67f7474
+    int i = 0; 
+    solve(7);
 	return 0;
 }
